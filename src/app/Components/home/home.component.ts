0b81f8f3b2c04661f0d7dd7a23531bbba8b30f0b
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-15 12:59:42 +0100

    FInished logic and view of filters for home component

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -8,6 +8,8 @@ import { PostService } from 'src/app/Services/post.service';
 import { SharedService } from 'src/app/Services/shared.service';
 
 import { FormatDatePipe } from 'src/app/Pipes/format-date.pipe';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { CategoryDTO } from 'src/app/Models/category.dto';
 
 @Component({
   selector: 'app-home',
@@ -16,16 +18,55 @@ import { FormatDatePipe } from 'src/app/Pipes/format-date.pipe';
 })
 export class HomeComponent {
   posts!: PostDTO[];
+  allCategories: CategoryDTO[];
+
+  titleFilter: FormControl;
+  descriptionFilter: FormControl;
+  publicationDateFilter: FormControl;
+  aliasFilter: FormControl;
+  categoriesFilter: FormControl;
+  filterForm: FormGroup;
+
   showButtons: boolean;
   constructor(
     private postService: PostService,
     private localStorageService: LocalStorageService,
     private sharedService: SharedService,
     private router: Router,
-    private headerMenusService: HeaderMenusService
+    private headerMenusService: HeaderMenusService,
+    private fb: FormBuilder
   ) {
     this.showButtons = false;
-    this.loadPosts();
+    this.allCategories = [];
+
+    this.titleFilter = new FormControl();
+    this.descriptionFilter = new FormControl();
+    this.publicationDateFilter = new FormControl();
+    this.aliasFilter = new FormControl();
+    this.categoriesFilter = new FormControl();
+
+    this.filterForm = this.fb.group({
+      titleFilter: this.titleFilter,
+      descriptionFilter: this.descriptionFilter,
+      publicationDateFilter: this.publicationDateFilter,
+      aliasfilter: this.aliasFilter,
+      categoriesFilter: this.categoriesFilter
+    });
+
+    this.loadPosts()
+    .then(() => {
+      const tmpCategories: CategoryDTO[][] = [...this.posts.map(post => post.categories)];
+      let allCategoriesUID: string[] = [];
+     
+      tmpCategories.forEach((array: CategoryDTO[]) => {
+        array.forEach((category: CategoryDTO) => {
+          if (!allCategoriesUID.includes(category.categoryId)) {
+            this.allCategories.push(category);
+            allCategoriesUID.push(category.categoryId);
+          }
+        })
+      })
+    });
   }
 
   ngOnInit(): void {
@@ -37,24 +78,6 @@ export class HomeComponent {
       }
     );
   }
-  private async loadPosts(): Promise<void> {
-    // TODO 2
-
-    let errorResponse: any;
-    const userId = this.localStorageService.get('user_id');
-
-    if (userId) {
-      this.showButtons = true;
-    }
-
-    try {
-      this.posts = await this.postService.getPosts();
-    } catch (error: any) {
-      errorResponse = error.error;
-      this.sharedService.errorLog(errorResponse);
-    }
-  }
-
   async like(postId: string): Promise<void> {
     let errorResponse: any;
     try {
@@ -76,4 +99,100 @@ export class HomeComponent {
       this.sharedService.errorLog(errorResponse);
     }
   }
+
+  async applyFilters(): Promise<void> {
+    await this.loadPosts();
+    
+    const filters = [
+      { filter: this.titleFilter.value, method: () => this.titleFilterMethod() },
+      { filter: this.descriptionFilter.value, method: () => this.descriptionFilterMethod() },
+      { filter: this.publicationDateFilter.value, method: () => this.publicationDateFilterMethod() },
+      { filter: this.aliasFilter.value, method: () => this.aliasFilterMethod() },
+      { filter: this.categoriesFilter.value, method: () => this.categoriesFilterMethod() },
+    ];
+  
+    filters.forEach(({ filter, method }) => {
+      if (filter) {
+        method();
+      }
+    });
+  }
+
+  private titleFilterMethod(): void {
+    this.posts = this.posts.filter((post: PostDTO) => post.title.includes(this.titleFilter.value));
+  }
+
+  private descriptionFilterMethod(): void {
+    this.posts = this.posts.filter((post: PostDTO) => post.description.includes(this.descriptionFilter.value));
+  }
+
+  private publicationDateFilterMethod(): void {
+    let tmpPosts: PostDTO[] = [];
+
+    this.posts.forEach((post: PostDTO, index: number) => {
+      const postDate: Date = new Date(post.publication_date);
+      let stringDate = '';
+
+      stringDate += postDate.getFullYear() + '-'
+
+      if ((+postDate.getMonth() + 1) < 10) {
+        stringDate += '0';
+      }
+
+      stringDate += (postDate.getMonth() + 1) + '-' + postDate.getDate();
+      
+      if (stringDate === this.publicationDateFilter.value) {
+        tmpPosts.push(post);
+      }
+
+      console.log(stringDate);
+      console.log(this.publicationDateFilter.value);
+
+      if (index === this.posts.length - 1) {
+        this.posts = tmpPosts;
+      }
+    });
+  }
+
+  private aliasFilterMethod(): void {
+    this.posts = this.posts.filter((post: PostDTO) => post.userAlias.includes(this.aliasFilter.value));
+  }
+
+  private categoriesFilterMethod(): void {
+    let tmpPosts: PostDTO[] = [];
+    // const allCategoriesUID: string[] = this.allCategories.map((category) => category.categoryId);
+
+    console.log(this.categoriesFilter.value)
+
+    this.posts.forEach((post: PostDTO, index: number) => {
+      let postCategoriesUID: string[] = post.categories.map((category: CategoryDTO) => category.categoryId);
+
+      if (this.categoriesFilter.value.every((uid: string) => postCategoriesUID.includes(uid))) {
+        tmpPosts.push(post);
+      }
+
+      if (index === this.posts.length - 1) {
+        this.posts = tmpPosts;
+      }
+    });
+  }
+
+  private async loadPosts(): Promise<void> {
+    // TODO 2
+
+    let errorResponse: any;
+    const userId = this.localStorageService.get('user_id');
+
+    if (userId) {
+      this.showButtons = true;
+    }
+
+    try {
+      this.posts = await this.postService.getPosts();
+    } catch (error: any) {
+      errorResponse = error.error;
+      this.sharedService.errorLog(errorResponse);
+    }
+  }
+
 }
